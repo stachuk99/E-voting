e65ba3eba5c5ed15ea9db0ddd3cc626edfda7a5a
Jan Majewski <jan.majewski@student.put.poznan.pl>
2021-12-21 19:51:42 +0100

    add votemodel

---
+++ EVoting_backend/Models/VoteModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace EVoting_backend.Models
+{
+    public class VoteModel
+    {
+        public int FormId { get; set; }
+        public List<SubFormModel> SubForms { get; set; }
+    }
+
+    public class SubFormModel
+    {
+        public int Id { get; set; }
+        public List<OptionModel> Options { get; set; }
+    }
+
+    public class OptionModel
+    {
+        public int Ident { get; set; }
+        public bool Checked { get; set; }
+    }
+}
